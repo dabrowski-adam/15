56ff115065095f7ad36698c1ba290c3ef6dd886b
Adam Dąbrowski <adam.dabrowski@htdevelopers.com>
2019-10-05 19:29:04 +0200

    Add main

--- puzzle.py
+++ puzzle.py
@@ -1,2 +1,10 @@
 def find_meaning_of_life_the_universe_and_everything() -> int:
     return 42
+
+def main():
+    answer = find_meaning_of_life_the_universe_and_everything()
+    message = f"The meaning of Lfe, the Universe, and Everything is: {answer}."
+    print(message)
+
+if __name__ == '__main__':
+    main()
