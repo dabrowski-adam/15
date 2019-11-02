1c7545b84bf91d2af5c6319f283a8554fcb0403e
Adam Dąbrowski <adam.dabrowski@htdevelopers.com>
2019-11-02 22:25:03 +0100

    Test show_solution

--- test_puzzle.py
+++ test_puzzle.py
@@ -1,6 +1,34 @@
 import pytest
-from puzzle import find_meaning_of_life_the_universe_and_everything
+from unittest.mock import Mock
+from puzzle import find_meaning_of_life_the_universe_and_everything, show_solution
 
 
 def test_find_meaning_of_life_the_universe_and_everything():
     assert find_meaning_of_life_the_universe_and_everything() == 42
+
+
+def test_show_solution_when_impossible_to_solve():
+    solution = None
+    output = Mock()
+
+    show_solution(solution, output)
+
+    output.write.assert_called_with("-1\n")
+
+
+def test_show_solution_when_already_solved():
+    solution = ""
+    output = Mock()
+
+    show_solution(solution, output)
+
+    output.write.assert_called_with("0\n")
+
+
+def test_show_solution_when_supplied_solution():
+    solution = "LRUD"
+    output = Mock()
+
+    show_solution(solution, output)
+
+    output.write.assert_called_with("4\nLRUD")
