94243bd1bc4cc855742d6d74c83df13ad0d5f531
Adam Dąbrowski <adam.dabrowski@htdevelopers.com>
2019-11-02 22:31:51 +0100

    Parametrize show_solution test

--- test_puzzle.py
+++ test_puzzle.py
@@ -7,28 +7,15 @@ def test_find_meaning_of_life_the_universe_and_everything():
     assert find_meaning_of_life_the_universe_and_everything() == 42
 
 
-def test_show_solution_when_impossible_to_solve():
-    solution = None
+show_solution_test_data = [
+    (None, "-1\n"),
+    ("", "0\n"),
+    ("LRUD", "4\nLRUD"),
+]
+
+
+@pytest.mark.parametrize("solution,expected", show_solution_test_data)
+def test_show_solution(solution, expected):
     output = Mock()
-
     show_solution(solution, output)
-
-    output.write.assert_called_with("-1\n")
-
-
-def test_show_solution_when_already_solved():
-    solution = ""
-    output = Mock()
-
-    show_solution(solution, output)
-
-    output.write.assert_called_with("0\n")
-
-
-def test_show_solution_when_supplied_solution():
-    solution = "LRUD"
-    output = Mock()
-
-    show_solution(solution, output)
-
-    output.write.assert_called_with("4\nLRUD")
+    output.write.assert_called_with(expected)
